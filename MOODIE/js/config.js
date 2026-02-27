01b57c2815364626f5db756d15d210f1e9e35eda
Heyuhhtm <24tec2cs173@vgu.ac.in>
2026-02-27 11:33:58 +0530

    fixed

--- MOODIE/js/config.js
+++ MOODIE/js/config.js
@@ -7,5 +7,5 @@ window.APP_CONFIG = {
   // Automatically determine if we're on localhost, 127.0.0.1, or opening locally via file://
   apiUrl: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.hostname === ''
     ? 'http://localhost:5000'
-    : 'https://diljourney-backend.onrender.com'
+    : 'https://diljourneybackend.onrender.com'
 };
